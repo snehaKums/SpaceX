c34e0e49a551d0bffd7e38bccb2e2b1b7551213c
Kumari <sneha.kumari@capgemini.com>
2021-01-14 12:10:34 +0530

    Updates

---
+++ precache-manifest.8445efa827e1b4c4f6dbd5c6480b42d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938bdf83c56084e48298dc11a46d7565",
+    "url": "/SpaceX/index.html"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/SpaceX/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "592463a168143ab24ebb",
+    "url": "/SpaceX/static/css/main.ad2c4c41.chunk.css"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/SpaceX/static/js/2.6cdbf0dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/SpaceX/static/js/2.6cdbf0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "592463a168143ab24ebb",
+    "url": "/SpaceX/static/js/main.85351711.chunk.js"
+  },
+  {
+    "revision": "dbbc612723065eb6204d",
+    "url": "/SpaceX/static/js/runtime-main.60a47493.js"
+  },
+  {
+    "revision": "c537d7d180515a2d14e723821dcf5097",
+    "url": "/SpaceX/static/media/loadRocket.c537d7d1.gif"
+  }
+]);
